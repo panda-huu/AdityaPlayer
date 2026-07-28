76de871a90b1004830a2dac365e3ee1bb1528d43
panda-huu <gurjasharma3@gmail.com>
2026-07-28 23:47:33 +0530

    Update pause.py

--- AdityaHalder/plugins/pause.py
+++ AdityaHalder/plugins/pause.py
@@ -1,20 +1,21 @@
 from pyrogram import filters
+
 from .. import bot, call, cdx
 from ..modules.helpers import AdminsOnlyWrapper
 
 
-@bot.on_message(cdx(["pause", "cpause"]) & \~filters.private)
+@bot.on_message(cdx("pause") & \~filters.private)
 @AdminsOnlyWrapper
 async def pause_vc_stream(client, message):
     chat_id = message.chat.id
     playing = call.queue.get(chat_id) or (chat_id in getattr(call, "active_chats", []))
     if not playing:
-        return await message.reply_text("**❌ Nothing Streaming.**")
+        return await message.reply_text("**Nothing Streaming.**")
     if await call.is_stream_off(chat_id):
-        return await message.reply_text("**✅ Already Paused.**")
+        return await message.reply_text("**Already Paused.**")
     try:
         await call.pause_stream(chat_id)
         await call.stream_off(chat_id)
-        return await message.reply_text("**⏸ Stream Paused.**")
+        return await message.reply_text("**Stream Paused.**")
     except Exception:
-        return await message.reply_text("**❌ Failed to pause.**")
+        return await message.reply_text("**Failed to pause.**")
