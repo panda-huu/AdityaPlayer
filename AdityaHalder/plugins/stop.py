fd7eef7accba5626035e07c87ecce1c756a62d7b
panda-huu <gurjasharma3@gmail.com>
2026-07-28 23:48:52 +0530

    Update stop.py

--- AdityaHalder/plugins/stop.py
+++ AdityaHalder/plugins/stop.py
@@ -1,9 +1,10 @@
 from pyrogram import filters
+
 from .. import bot, call, cdx
 from ..modules.helpers import AdminsOnlyWrapper
 
 
-@bot.on_message(cdx(["end", "stop", "cend"]) & \~filters.private)
+@bot.on_message(cdx(["end", "stop"]) & \~filters.private)
 @AdminsOnlyWrapper
 async def stop_vc_stream(client, message):
     try:
@@ -13,9 +14,9 @@ async def stop_vc_stream(client, message):
     chat_id = message.chat.id
     playing = call.queue.get(chat_id) or (chat_id in getattr(call, "active_chats", []))
     if not playing:
-        return await message.reply_text("**❌ Nothing Streaming.**")
+        return await message.reply_text("**Nothing Streaming.**")
     try:
         await call.close_stream(chat_id)
-        return await message.reply_text("**⏹ Streaming Stopped.**")
+        return await message.reply_text("**Streaming Stopped.**")
     except Exception:
-        return await message.reply_text("**❌ Failed to stop.**")
+        return await message.reply_text("**Failed to stop.**")
