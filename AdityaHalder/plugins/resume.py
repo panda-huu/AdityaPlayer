6d4d19a27d2d4d684eefbad734075fd7a3f0cf9a
panda-huu <gurjasharma3@gmail.com>
2026-07-28 23:48:13 +0530

    Update resume.py

--- AdityaHalder/plugins/resume.py
+++ AdityaHalder/plugins/resume.py
@@ -1,20 +1,21 @@
 from pyrogram import filters
+
 from .. import bot, call, cdx
 from ..modules.helpers import AdminsOnlyWrapper
 
 
-@bot.on_message(cdx(["resume", "cresume"]) & \~filters.private)
+@bot.on_message(cdx("resume") & \~filters.private)
 @AdminsOnlyWrapper
 async def resume_vc_stream(client, message):
     chat_id = message.chat.id
     playing = call.queue.get(chat_id) or (chat_id in getattr(call, "active_chats", []))
     if not playing:
-        return await message.reply_text("**❌ Nothing Streaming.**")
+        return await message.reply_text("**Nothing Streaming.**")
     if not await call.is_stream_off(chat_id):
-        return await message.reply_text("**✅ Already Playing.**")
+        return await message.reply_text("**Already Playing.**")
     try:
         await call.resume_stream(chat_id)
         await call.stream_on(chat_id)
-        return await message.reply_text("**▶️ Stream Resumed.**")
+        return await message.reply_text("**Stream Resumed.**")
     except Exception:
-        return await message.reply_text("**❌ Failed to resume.**")
+        return await message.reply_text("**Failed to resume.**")
