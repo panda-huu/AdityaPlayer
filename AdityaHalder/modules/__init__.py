45bc9bcd637c4dddf35c9979a21929ac92e69fde
panda-huu <gurjasharma3@gmail.com>
2026-07-28 22:39:13 +0530

    Update __init__.py

--- AdityaHalder/modules/__init__.py
+++ AdityaHalder/modules/__init__.py
@@ -3,7 +3,7 @@ from .. import console
 
 def check_variables():
     if console.API_ID == 0:
-        console.logs(__name__).info("❌ 'API ID' - Not found❗")
+        console.logs(__name__).info("❌ 'API_ID' - Not found❗")
         sys.exit()
     if not console.API_HASH:
         console.logs(__name__).info("❌ 'API_HASH' - Not found❗")
@@ -11,17 +11,11 @@ def check_variables():
     if not console.BOT_TOKEN:
         console.logs(__name__).info("❌ 'BOT_TOKEN' - Not found❗")
         sys.exit()
-    if (
-        not console.STRING1
-        and not console.STRING2
-        and not console.STRING3
-        and not console.STRING4
-        and not console.STRING5
-    ):
+    if not any([console.STRING1, console.STRING2, console.STRING3, console.STRING4, console.STRING5]):
         console.logs(__name__).info("❌ 'STRING_SESSION' - Not found❗")
         sys.exit()
-    if not console.MONGO_URL:
-        console.logs(__name__).info("❌ 'MONGO_URL' - Not found❗")
+    if not console.DATABASE_URL or not console.DATABASE_PASSWORD:
+        console.logs(__name__).info("❌ 'DATABASE_URL' or 'DATABASE_PASSWORD' - Not found❗")
         sys.exit()
     if console.OWNER_ID == 0:
         console.logs(__name__).info("❌ 'OWNER_ID' - Not found❗")
